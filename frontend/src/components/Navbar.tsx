9529a7ffe58c37db7cdef1f899dffdd9ac6e3318
jmstover05 <jmstover3315@gmail.com>
2026-07-29 17:47:30 -0400

    Updated the Navbar
    added some designs to the Navbar w/ icons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { faRightFromBracket, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingHeart, faHouse, faLightbulb, faRightFromBracket, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../lib/useAuth";
@@ -16,14 +16,18 @@ const Navbar = () => {
 
   return (
     <div className="bg-emerald-800 w-full flex flex-row p-5 text-white text-2xl">
+      <p><b>CATALYST</b>&emsp;|&emsp;</p>
       {isAuthenticated ? (
         <>
           <div className={"gap-4 flex flex-row"}>
+            <NavLink to="/" className={activeNavClass}>
+              Home <FontAwesomeIcon icon={faHouse} className="relative top-px" />
+            </NavLink>
             <NavLink to="/volunteers" className={activeNavClass}>
-              Volunteers
+              Volunteers <FontAwesomeIcon icon={faHandHoldingHeart} className="relative top-px" />
             </NavLink>
             <NavLink to="/opportunities" className={activeNavClass}>
-              Opportunities
+              Opportunities <FontAwesomeIcon icon={faLightbulb} className="relative top-px" />
             </NavLink>
           </div>
           <div className={"ml-auto"}>
