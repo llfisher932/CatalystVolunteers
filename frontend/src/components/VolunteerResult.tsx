9f392ceb6581b9f34acc2166fbbb81c496873c9d
jmstover05 <jmstover3315@gmail.com>
2026-08-04 19:58:14 -0400

    Added VolunteerResult Component
    Added a component that can display volunteer info in <td> elements to be used in the Volunteers page.

---
+++ frontend/src/components/VolunteerResult.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type VolunteerResultProps = {
+    fName: string;
+    lName: string;
+    email: string;
+    approvalStatus: string;
+}
+
+const VolunteerResult = ({fName, lName, email, approvalStatus}:VolunteerResultProps) => {
+
+    /* Volunteer Restult Table Formatting:
+        firstName | lastName | email | approvalStatus | Edit (takes you to HOME for now)
+
+    */
+    return(
+        <>
+        
+            <td>{fName}</td>
+            <td>{lName}</td>
+            <td>{email}</td>
+            <td>{approvalStatus}</td>
+            {/* Reroute to edit page here (routes to home page for now)*/}
+            <td><Link to="/"> <FontAwesomeIcon icon={faPenToSquare} className="relative top-px" /></Link></td>
+        
+        </>
+    );
+}
+
+export default VolunteerResult;
